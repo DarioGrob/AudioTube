73b850e944105cbc75f99c3f5e64d040c1667680
DarioGrob <dario.grob@gibmit.ch>
2020-10-14 22:03:27 +0200

    created Video to Audio converter Service

---
+++ src/main/java/com/modul152/projekt/backend/service/VideoToAudioConverter.java
@@ -0,0 +1,55 @@
+package com.modul152.projekt.backend.service;
+
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Service
+public class VideoToAudioConverter {
+
+    public int convert(String youtubeId) {
+        String videoFileName = "";
+        Path dir = Paths.get("C:\\Users\\Dario Grob\\Desktop\\Schule\\Module\\152\\Projekt\\audiotube\\youtubeVideo");
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*" + youtubeId + "*")) {
+            for (Path entry: stream) {
+                videoFileName = entry.getFileName().toString();
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            String videoFile = "C:\\Users\\Dario Grob\\Desktop\\Schule\\Module\\152\\Projekt\\audiotube\\youtubeVideo\\" + videoFileName;
+            String[] splitVideoFileName = videoFileName.split("\\.");
+            String newAudioFile = "C:\\Users\\Dario Grob\\Desktop\\Schule\\Module\\152\\Projekt\\audiotube\\youtubeAudio\\" + splitVideoFileName[0] + ".mp3";
+
+            Runtime runtime = Runtime.getRuntime();
+            String[] command = new String[]{
+                    "C:\\Users\\Dario Grob\\Desktop\\test\\ffmpeg.exe",
+                    "-i",
+                    videoFile,
+                    newAudioFile,
+            };
+            String line;
+            Process process = runtime.exec(command, new String[]{}, new File("C:\\Users\\Dario Grob\\Desktop\\Schule\\Module\\152\\Projekt\\audiotube\\youtubeAudio"));
+            BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while ((line = input.readLine()) != null) {
+                System.out.println(line);
+            }
+            input.close();
+            process.waitFor();
+            System.out.println(process.exitValue());
+            return process.exitValue();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            return 1;
+        }
+    }
+}
--- src/main/java/com/modul152/projekt/views/playlist/PlaylistPresenter.java
+++ src/main/java/com/modul152/projekt/views/playlist/PlaylistPresenter.java
@@ -1,6 +1,6 @@
 package com.modul152.projekt.views.playlist;
 
-import com.modul152.projekt.backend.service.ConvertMp4ToMp3;
+import com.modul152.projekt.backend.service.VideoToAudioConverter;
 import com.modul152.projekt.backend.service.DownloadYoutube;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,10 +9,10 @@ import org.springframework.stereotype.Component;
 public class PlaylistPresenter {
 
     private final DownloadYoutube downloadYoutube;
-    private final ConvertMp4ToMp3 converter;
+    private final VideoToAudioConverter converter;
 
     @Autowired
-    public PlaylistPresenter(DownloadYoutube downloadYoutube, ConvertMp4ToMp3 converter) {
+    public PlaylistPresenter(DownloadYoutube downloadYoutube, VideoToAudioConverter converter) {
         this.downloadYoutube = downloadYoutube;
         this.converter = converter;
     }
